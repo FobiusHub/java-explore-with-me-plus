645c6f29d42ca533112004509abcb6695ecbf443
Egor Malyshev <ya.krumm@ya.ru>
2025-11-07 23:27:15 +0300

    fix_1 Дополнена валидация полей name и email для UserDto

--- ewm_service/src/main/java/ru/practicum/ewm/service/user/dto/UserDto.java
+++ ewm_service/src/main/java/ru/practicum/ewm/service/user/dto/UserDto.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm.service.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +16,11 @@ public class UserDto {
     private Long id;
 
     @NotBlank(message = "Имя должно быть заполнено")
+    @Size(min = 2, max = 250)
     private String name;
 
     @Email(message = "Некорректный email")
     @NotBlank(message = "Некорректный email")
+    @Size(min = 6, max = 254)
     private String email;
 }
